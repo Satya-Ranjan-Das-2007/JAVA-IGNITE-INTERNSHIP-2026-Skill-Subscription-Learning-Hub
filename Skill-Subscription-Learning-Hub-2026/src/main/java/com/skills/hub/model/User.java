d0fb8f07d41cf2403006d2b7bbaa44b1ef9bd60d
25bcsi56_satya ranjan das <satyaranjanchinu@gmail.com>
2026-06-18 22:35:58 +0530

    Update User.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/model/User.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/model/User.java
@@ -29,8 +29,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
 
